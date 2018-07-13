81350dfcb983a045d11a145d5b8ee65e06d7a300
KePing Zang <dotping@online.de>
2018-07-13 15:52:53 +0200

    Build four JavaScript objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,47 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var bio = {
+    name : string,
+    role : string,
+    contacts: { 
+          mobile: string,
+          email: string ,
+          github: string,
+          twitter: string ,
+          location: string},
+    welcomeMessage: string ,
+    skills: [],
+    biopic:  url,
+    display: function (){}}
+var education = {
+    schools: [{name: string,
+        location: string,
+        degree: string,
+        majors: [],
+        dates: string,
+        url: string }],
+         
+    onlineCourses: [{title: string,
+        school: string,
+        dates: string ,
+        url: string}],
+         
+    display: function (){}
+}
+var work = {
+    jobs: [{employer: string, 
+        title: string, 
+        location: string ,
+        dates: string ,
+        description: string }],
+    
+display: function (){}}
+var projects = {
+    projects: [{title: string ,
+        dates: string ,
+        description: string,
+        images: [url]}],
+          
+    display: function (){}
+}
